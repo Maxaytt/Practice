dcd289121b32ddd0fbcef264d5ab9e2889910e12
fabileoo <fabishleo@gmail.com>
2024-07-04 13:08:58 +0200

    feat: add DbSet Answer Question

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Formats.Asn1;
+using Microsoft.EntityFrameworkCore;
+using Domain.Models;
 
 namespace Infrastructure
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        
+        public virtual DbSet<Answer> Answers {get;set;}
+        public virtual  DbSet<Question> Questions{get;set;}
     }
 }
