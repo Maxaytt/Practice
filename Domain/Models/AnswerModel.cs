0b095c743f6b2157740c03021055ad54e1965814
fabileoo <fabishleo@gmail.com>
2024-07-04 13:25:35 +0200

    feat: delet nullable and requared

--- Domain/Models/AnswerModel.cs
+++ Domain/Models/AnswerModel.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 public class Answer 
 {
-    public int? Id {get; set;}
-    public int? QuestionId {get;set;}
-    public Question? Question {get; set;}
-    public required string  Text {get;set;}
+    public int Id {get; set;}
+    public int QuestionId {get;set;}
+    public Question Question {get; set;}
+    public  string  Text {get;set;}
 
     public bool IsTrue {get; set;}
 }
--- Domain/Models/QuestionModel.cs
+++ Domain/Models/QuestionModel.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 public class Question 
 {
-    [Required]
+    
     public int Id  {get; set;}
-    public required string Text {get; set;}
+    public string Text {get; set;}
 
     
-    public required ICollection<Answer> Answers {get; set;}
+    public  ICollection<Answer> Answers {get; set;}
 }
