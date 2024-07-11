e81f855295e729db2483beb4b57b149afb1319ba
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-07-11 14:06:44 +0200

    feature: Added Model Image

---
+++ Domain/Models/ImageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Image
+    {
+        Guid Guid { get; set; }
+        public byte[] Content { get; set; }
+   
+        //Caption which is displayed when image cannot be loaded
+        public string Caption { get; set; } 
+
+        public ICollection<Film> Films { get; set; }
+     
+    }
+}
