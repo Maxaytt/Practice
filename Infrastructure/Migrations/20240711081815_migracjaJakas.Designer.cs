fd1f1b456e84df6424de5dbc74c9d0d5a70df064
fabileoo <fabishleo@gmail.com>
2024-07-11 13:14:42 +0200

    fix: metod BuildTargetModel

--- Infrastructure/Migrations/20240711081815_migracjaJakas.Designer.cs
+++ Infrastructure/Migrations/20240711081815_migracjaJakas.Designer.cs
@@ -16,7 +16,7 @@ namespace Infrastructure.Migrations
     partial class migracjaJakas
     {
         /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
