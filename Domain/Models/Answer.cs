0cbd04e41954b5cf0b974250e893554dafdfe4f9
Maxaytt <Maxaytt@gmail.com>
2024-07-12 10:32:15 +0200

    refactor: rename answer and question models

--- Domain/Models/Answer.cs
+++ Domain/Models/Answer.cs
@@ -5,8 +5,8 @@ public class Answer
 {
     public int Id {get; set;}
     public int QuestionId {get;set;}
-    public Question Question {get; set;}
-    public  string  Text {get;set;}
+    public Question Question { get; set; } = null!;
+    public string Text { get; set; } = null!;
 
     public bool IsTrue {get; set;}
 }
