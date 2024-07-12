8d17e8089cb3c92d4b9c3cdcd01cd4484955a38b
fabileoo <fabishleo@gmail.com>
2024-07-12 14:46:11 +0200

    feat: add httpget httppost

--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -1,18 +1,66 @@
 using Microsoft.AspNetCore.Mvc;
 using Domain.Models;
-using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Infrastructure;
 
-
-namespace Web.Controllers 
+namespace Web.Controllers
 {
     public class UserController : Controller
     {
+        private readonly AppDbContext _context;
 
-        public IActionResult Index()
-        { 
-            return View();
+        public UserController(AppDbContext context)
+        {
+            _context = context;
         }
 
-     
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var userIdString = "A845927D-D24A-4C89-9259-177E4D788E52";
+
+            if (!Guid.TryParse(userIdString, out var userId))
+            {
+                return BadRequest("Invalid user ID format.");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);;
+
+            if (user == null)
+            {
+                return NotFound($"User with ID '{userId}' not found.");
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == model.Id);
+                if (user == null)
+                {
+                    return NotFound($"User with ID '{model.Id}' not found.");
+                }
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+                user.Gmina = model.Gmina;
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+     // Проблемма с пост , не сохраняет в БД
+       
     }
 }
+
+
