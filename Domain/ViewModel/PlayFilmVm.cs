6f22f2eb23805e24a3093ee522713b76061a2850
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-07-18 09:36:04 +0200

    feature: added PlayFilmVm

---
+++ Domain/ViewModel/PlayFilmVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModel
+{
+    public class PlayFilmVm
+    {
+        public Guid Id { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string Name { get; set; }
+    }
+}
