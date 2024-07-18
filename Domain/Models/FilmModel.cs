ab1eac9a2729e44bdbf5b1de6403b29ddfdc72e3
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-07-18 09:29:44 +0200

    Refactor: made relation from to optional to required

--- Domain/Models/FilmModel.cs
+++ Domain/Models/FilmModel.cs
@@ -10,12 +10,18 @@ namespace Domain.Models
     public class Film
     {
         public Guid Id { get; set; }
-        public byte[] Content { get; set; } = null!;
-        public string Name { get; set; } = null!;
-        public ICollection<Question> Questions { get; set; } = null!;
+        
+        public byte[] Content { get; set; }
+        
+        // For example video/mp4 or video/avi
+        public string ContentType { get; set; }
+        
+        public string Name { get; set; }
+        
+        public ICollection<Question> Questions { get; set; }
 
+        public Image Image { get; set; }
 
-        public Image Image { get; set; } = null!;
         public Guid ImageId { get; set; }
     }
 }
