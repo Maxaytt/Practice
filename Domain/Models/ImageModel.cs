a507ec7cf9f2d324df7d52903446582f5c56a026
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-07-18 09:30:59 +0200

    Refactor: changing relation Images:Films from 1:n to 1:1

--- Domain/Models/ImageModel.cs
+++ Domain/Models/ImageModel.cs
@@ -9,12 +9,17 @@ namespace Domain.Models
     public class Image
     {
         public Guid Id { get; set; }
+
         public byte[] Content { get; set; }
-   
+
+        // For example image/jpg or image/png
+        public string ContentType { get; set; }
+
         //Caption which is displayed when image cannot be loaded
         public string Caption { get; set; } 
 
-        public ICollection<Film> Films { get; set; }
+        public Film Film { get; set; }
+
      
     }
 }
