4cd2518a5a265ee43bc062126bddbea414adac2c
milosz.niewiarowski.4@gmail.com <milosz.niewiarowski.4@gmail.com>
2024-07-19 14:19:41 +0200

    Refactor: adding Id property to CreareEditFilmVm

--- Domain/ViewModel/CreateEditFilmVm.cs
+++ Domain/ViewModel/CreateEditFilmVm.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Http;
 using System.Web;
 namespace Domain.ViewModel
 {
-    public class FilmEditOrUploadVM
+    public class CreateEditFilmVm
     {
+        public Guid Id { get; set; } = new Guid();
         public IFormFile VideoFile { get; set; } = null!;
         public IFormFile ImageFile { get; set; } = null!;
         public string Name { get; set; } = null!;
