e68143572c81b55747eadcb374381e3d9e55765a
Piskunov <nikolajpiskunov14@gmail.com>
2024-07-27 11:31:05 +0200

    feat: add view models

---
+++ Domain/ViewModel/AnswerVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModel
+{
+    public class AnswerVm
+    {
+        public Guid Id;
+        public string Text { get; set; }
+        public bool IsTrue { get; set; }
+    }
+}
---
+++ Domain/ViewModel/QuestionVm.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModel
+{
+    public class QuestionVm
+    {
+        public Guid FilmId { get; set; }
+        public string Text { get; set; }
+        public List<AnswerVm> Answers { get; set; } = new List<AnswerVm>();
+        public Guid Id { get; set; }
+    }
+}
